ba8a73aa971ebf09301ef6a19e9a2cd124e14ebb
Neon T. Flame <neontflame@gmail.com>
2025-05-26 16:23:27 -0300

    example of how attack() can be used!!
    yeayaeyaeyaey
    also sonic's spritesheet doesnt SUCK ASS now

---
+++ assets/gameplay/fighter/sonic/attacks.hx
@@ -0,0 +1,17 @@
+function update() {
+
+	if (InputCoolio.key('right') && InputCoolio.key('basic', true)) {
+	
+		// SAMPLE RIGHT ATTACK
+		fit.attack(function() {
+			fit.fitSprite.animPlay('run');
+			fit.hitbox.velocity.x = 100;
+			
+			new FlxTimer().start(0.25, function(tmr:FlxTimer) {
+				fit.status = "default";
+			});
+		
+		});
+	}
+	
+}
---
+++ assets/gameplay/fighter/sonic/init.hx
@@ -0,0 +1,17 @@
+function createPost() {
+	fit.hitbox.changeSize(59, 105);
+	fit.fitSprite.hitboxOffset = [0, 30];
+	trace(fit.hitbox);
+}
+
+function loadAnims() {
+	fit.fitSprite.animAdd('idle', 'sonic idle', [0, 0]);
+	fit.fitSprite.animAdd('walk', 'sonic walk', [0, 0]);
+	fit.fitSprite.animAdd('run', 'sonic run', [0, 0], 24, true);
+	
+	fit.fitSprite.animAdd('jump', 'sonic spin', [0, 0], 30);
+	fit.fitSprite.animAdd('fall', 'sonic fall', [0, 0]);
+	
+	trace(fit.fitSprite.animOffsets);
+	fit.fitSprite.animPlay('idle');
+}
