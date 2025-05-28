2869dddd2be6dc07fa02d4f050e97b8866c76111
Neon T. Flame <neontflame@gmail.com>
2025-05-28 02:10:11 -0300

    damage (i think!)
    i just want everyone reading this to know that i havent tested ANYTHING in this i just coded as i went and occasionally looked at the GMS2 code i wrote which was. Not very helpful but we ball

--- source/objects/Fighter.hx
+++ source/objects/Fighter.hx
@@ -5,6 +5,8 @@ import flixel.FlxSprite;
 import flixel.group.FlxSpriteGroup;
 import flixel.util.FlxDirectionFlags;
 import flixel.util.FlxTimer;
+import flixel.math.FlxMath;
+import flixel.math.FlxAngle;
 
 import backend.IrisHandler;
 
@@ -27,6 +29,7 @@ class Fighter extends FlxSpriteGroup
 	
 	public var FLOOR_FRICTION:Float = 6;
 	public var AIRDODGE_COEFFICIENT:Float = 4.5;
+	public var AIR_MOVEMENT_COEFFICIENT:Float = 5;
 	
 	public var RUNNING:Bool = false;
 	
@@ -97,17 +100,35 @@ class Fighter extends FlxSpriteGroup
 	public var horizontalDI:Float = 0;
 	public var verticalDI:Float = 0;
 	
+	public var hitstun:Float = 0;
+	public var hurtTimer:Float = 0;
+	
 	var airdodgeTimer:FlxTimer;
 	
 	override public function update(elapsed:Float)
 	{
 		fitScript.call('update', [elapsed]);
 		
-		playerMovement();
+		hitStunner();
+		
+		if (hitstun <= 0)
+			playerMovement();
+			
 		super.update(elapsed);
 		
 		fitScript.call('updatePost', [elapsed]);
 		
+	}
+	
+	function hitStunner() {
+		if (hitstun > 0) {
+			hitbox.velocity.x = 0;
+			hitbox.velocity.y = 0;
+			hitbox.acceleration.y = 0;
+			hitbox.acceleration.x = 0;
+			
+			hitstun -= 1;
+		}
 	}
 	
 	function playerMovement() {
@@ -125,11 +146,66 @@ class Fighter extends FlxSpriteGroup
 		
 		// start player movement
 		switch (status) {
-			case "dmg" | "dmgcontrollable":
+			case "dmg" | "dmgcontrollable": 
 				// dmg/dmgcont
-				// they share much of the same code, it's just now we gotta figure out what DMGControllable does specifically
-				if (status == "dmgcontrollable") {
-					// woooah
+				// they share much of the same code, it's just now we gotta figure out what DMG and DMGControllable do specifically
+				
+				var airvel = 0;
+				var horvel = 0;
+				
+				if (!hitbox.isTouching(FlxDirectionFlags.FLOOR)) {
+					airvel = hitbox.velocity.y;
+				}
+				if (!hitbox.isTouching(FlxDirectionFlags.WALL)) {
+					horvel = hitbox.velocity.x;
+				}
+				if (hitbox.isTouching(FlxDirectionFlags.CEILING)) {
+					hitbox.velocity.y = Math.abs(airvel) * 1.05; // this one is the only one that gets stronger cuz i thought itd be funny
+				} 
+				
+				if (hitbox.isTouching(FlxDirectionFlags.LEFT)) {
+					hitbox.velocity.x = Math.abs(horvel) * 0.95;
+				} 
+				if (hitbox.isTouching(FlxDirectionFlags.RIGHT)) {
+					hitbox.velocity.x = -Math.abs(horvel) * 0.95;
+				} 
+				
+				if (status == 'dmg') {
+					hitbox.maxVelocity.x = 0;
+					hitbox.drag.x = WALK_SPEED;
+					
+					// funny bounce !
+					if (hitbox.isTouching(FlxDirectionFlags.FLOOR)) {
+						hitbox.velocity.y = -Math.abs(airvel) * 0.95;
+					} 
+					
+					// cant move on damage silly boy !
+					if (hurtTimer > 0)
+						hurtTimer -= 1;
+						
+					if (hurtTimer <= 0)
+						status = 'dmgcontrollable';
+				}
+				
+				if (status == 'dmgcontrollable') {
+					hitbox.maxVelocity.x = WALK_SPEED;
+					HORIZONTAL_ACCEL = WALK_SPEED * (AIR_MOVEMENT_COEFFICIENT * 0.85);
+					hitbox.drag.x = WALK_SPEED;
+					hitbox.drag.y = 0;
+					
+					// no bounce
+					if (hitbox.isTouching(FlxDirectionFlags.FLOOR)) {
+						status = 'default';
+					} 
+					
+					// can move !
+					if (InputCoolio.key('right')) {
+						hitbox.acceleration.x = HORIZONTAL_ACCEL;
+					} else if (InputCoolio.key('left')) {
+						hitbox.acceleration.x = -HORIZONTAL_ACCEL;
+					} else {
+						hitbox.acceleration.x = 0;
+					}
 				}
 				
 			case "airdodge":
@@ -151,7 +227,7 @@ class Fighter extends FlxSpriteGroup
 				// i Just remembered aerial attacks are a thing fuack
 				if (controlOnAttack) {
 					hitbox.maxVelocity.x = WALK_SPEED;
-					HORIZONTAL_ACCEL = WALK_SPEED * 6;
+					HORIZONTAL_ACCEL = WALK_SPEED * AIR_MOVEMENT_COEFFICIENT;
 					// hitbox.drag.x = WALK_SPEED * FLOOR_FRICTION;
 					hitbox.drag.y = 0;
 					
@@ -299,6 +375,19 @@ class Fighter extends FlxSpriteGroup
 		} else {
 			trace("You can't attack someone without a function to do so!!");
 		}
+	}
+	
+	function damage(angle, damage, knockback, _hurtFrames, _hitstun){
+		var the_thing:Float = FlxMath.lerp(dmg_percent, 100, 0.3);
+
+		status = "dmg";
+		hitstun = _hitstun;
+		
+		dmgPercent += damage;
+		hitbox.velocity.x = (the_thing * knockback) * (Math.sin(angle * FlxAngle.TO_RAD) + horizontalDI);
+		hitbox.velocity.y = (the_thing * knockback) * ((Math.cos(angle * FlxAngle.TO_RAD) + verticalDI) * -1);
+		
+		hurtTimer = _hurtFrames;
 	}
 	
 }
